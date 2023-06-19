865417cb22d014afbdfaf9015851463f2c008f5b
longtingho2010 <128117357+longtingho2010@users.noreply.github.com>
2023-06-19 16:51:34 +0800

    added movielist page

--- src/Components/Carousel1/Carousel1.js
+++ src/Components/Carousel1/Carousel1.js
@@ -2,6 +2,7 @@ import React from "react";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from "react-responsive-carousel";
 import "./Carousel1.css";
+import { Link } from "react-router-dom";
 
 function Carousel1({ posts }) {
   return (
@@ -14,23 +15,28 @@ function Carousel1({ posts }) {
       autoFocus={true}
     >
       {posts.map((post) => (
-        <div className="image-silder" key={post.id}>
-          <img
-            alt=""
-            src={`https://image.tmdb.org/t/p/original/${post.backdrop_path}`}
-          />
-          <div className="image-silder-overlay">
-            <div className="post-title">
-              {post.title}
-              <span className="post-rating">
-                Rate: {post.vote_average}
-                <i className="fa-solid fa-star-sharp"></i>
-              </span>
-            </div>
+        <Link
+          style={{ textDecoration: "none", color: "#fff" }}
+          to={`/movie/${post.id}`}
+        >
+          <div className="image-silder" key={post.id}>
+            <img
+              alt=""
+              src={`https://image.tmdb.org/t/p/original/${post.backdrop_path}`}
+            />
+            <div className="image-silder-overlay">
+              <div className="post-title">
+                {post.title}
+                <span className="post-rating">
+                  Rate: {post.vote_average}
+                  <i className="fa-solid fa-star-sharp"></i>
+                </span>
+              </div>
 
-            <div className="post-description">{post.overview}</div>
+              <div className="post-description">{post.overview}</div>
+            </div>
           </div>
-        </div>
+        </Link>
       ))}
     </Carousel>
   );
