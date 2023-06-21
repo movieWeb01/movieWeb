0bd1b89760eecefb3748c21ba741c672765c4121
ccm19981230 <130039612+ccm19981230@users.noreply.github.com>
2023-06-21 20:18:31 +0800

    Added Today's Trending Container (Rex)

--- src/Components/Containers/Container.jsx
+++ src/Components/Containers/Container.jsx
@@ -7,6 +7,8 @@ function Container({ posts }) {
   const [topRated, setTopRated] = useState([]); 
   const [upComing, setUpComing] = useState([]); 
   const [nowPlaying, setNowPlaying] = useState([]); 
+  const [trending, setTrending] = useState([]); 
+
   const [favorite, setFavorite] = useState(() => JSON.parse(localStorage.getItem("favorite")) || []);
 
   useEffect(() => { 
@@ -44,7 +46,19 @@ function Container({ posts }) {
         console.log(res);
         setNowPlaying(res.data.results);
       });
+  }, []); 
+
+  useEffect(() => {
+    axios
+      .get(
+        `https://api.themoviedb.org/3/trending/movie/day?api_key=6a3a9e9a61085d657b30d36d1c7b5ba7`
+      )
+      .then((res) => {
+        console.log(res);
+        setTrending(res.data.results);
+      });
   }, []);
+
 
   var slider1holder = document.getElementById("slider1");
   var photobannerholder = document.getElementsByClassName("photobanner")[0];
@@ -127,6 +141,99 @@ function Container({ posts }) {
                 </p>
               </a>
             </Link>
+          ))}
+        </div>
+      </div>
+
+      <h1 className="slider_title">Today's Trending</h1>
+      <div>
+        <div className="flex-parent">
+          {trending.map((post) => (
+            <div className="moviebox">
+            <Link to={`/movie/${post.id}`} style={{ textDecoration: "none" }}>
+              <a
+                target="_blank"
+                style={{ textDecoration: "none", color: "#fff", fontFamily: "Roboto" }}
+              >
+                <img
+                  className="movieimg"
+                  src={`https://image.tmdb.org/t/p/original/${post.poster_path}`}
+                />
+                <p className="caption">{`${post.original_title}`}</p>
+                <div className="star-rating">
+                  {post.vote_average >= 2 ? (
+                    <img
+                      width="20px"
+                      src="https://img.uxwing.com/wp-content/themes/uxwing/download/arts-graphic-shapes/star-icon.svg"
+                    />
+                  ) : (
+                    <img
+                      width="20px"
+                      src="https://img.uxwing.com/wp-content/themes/uxwing/download/arts-graphic-shapes/star-full-icon.svg"
+                    />
+                  )}
+                  {post.vote_average >= 4 ? (
+                    <img
+                      width="20px"
+                      src="https://img.uxwing.com/wp-content/themes/uxwing/download/arts-graphic-shapes/star-icon.svg"
+                    />
+                  ) : (
+                    <img
+                      width="20px"
+                      src="https://img.uxwing.com/wp-content/themes/uxwing/download/arts-graphic-shapes/star-full-icon.svg"
+                    />
+                  )}
+                  {post.vote_average >= 6 ? (
+                    <img
+                      width="20px"
+                      src="https://img.uxwing.com/wp-content/themes/uxwing/download/arts-graphic-shapes/star-icon.svg"
+                    />
+                  ) : (
+                    <img
+                      width="20px"
+                      src="https://img.uxwing.com/wp-content/themes/uxwing/download/arts-graphic-shapes/star-full-icon.svg"
+                    />
+                  )}
+                  {post.vote_average >= 8 ? (
+                    <img
+                      width="20px"
+                      src="https://img.uxwing.com/wp-content/themes/uxwing/download/arts-graphic-shapes/star-icon.svg"
+                    />
+                  ) : (
+                    <img
+                      width="20px"
+                      src="https://img.uxwing.com/wp-content/themes/uxwing/download/arts-graphic-shapes/star-full-icon.svg"
+                    />
+                  )}
+                  {post.vote_average >= 10 ? (
+                    <img
+                      width="20px"
+                      src="https://img.uxwing.com/wp-content/themes/uxwing/download/arts-graphic-shapes/star-icon.svg"
+                    />
+                  ) : (
+                    <img
+                      width="20px"
+                      src="https://img.uxwing.com/wp-content/themes/uxwing/download/arts-graphic-shapes/star-full-icon.svg"
+                    />
+                  )}
+                  {post.vote_average !== 0 && ` (${post.vote_average})`}
+                </div>
+              </a>
+            </Link>
+            <Link>
+              <button className='heart_btn' onClick={() => heartFunction(post.id)}>
+                  {favorite.includes(post.id) ? 
+                  <img
+                    className="heart_img"
+                    src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/42/Love_Heart_SVG.svg/968px-Love_Heart_SVG.svg.png"
+                  />
+                   : <img
+                   className="heart_img_black"
+                   src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/42/Love_Heart_SVG.svg/968px-Love_Heart_SVG.svg.png"
+                 />}
+              </button>
+            </Link>
+            </div>
           ))}
         </div>
       </div>
@@ -320,7 +427,8 @@ function Container({ posts }) {
           ))}
         </div>
       </div>
-
+      
+      
     </div>
   );
 }
