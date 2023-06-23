b55d0d127cf52688f885a9b9f12e2a1b104c61a9
longtingho2010 <128117357+longtingho2010@users.noreply.github.com>
2023-06-23 13:31:12 +0800

    updated loading anmation on genres page

--- src/Components/MoviePage/MoviePage.js
+++ src/Components/MoviePage/MoviePage.js
@@ -15,28 +15,6 @@ const MoviePage = () => {
     () => JSON.parse(localStorage.getItem("favorite")) || []
   );
 
-  // useEffect(() => {
-  //   // const getMovie = async () => {
-  //   //   setIsLoading(true);
-  //   //   try {
-  //   //     const res = axios.get(
-  //   //       `https://api.themoviedb.org/3/movie/${type}?api_key=6a3a9e9a61085d657b30d36d1c7b5ba7`
-  //   //     );
-  //   //     setMovieList(res.data.results);
-  //   //   } catch (error) {}
-  //   //   setIsLoading(false);
-  //   // };
-  //   // getMovie();
-  //   axios
-  //     .get(
-  //       `https://api.themoviedb.org/3/movie/${type}?api_key=6a3a9e9a61085d657b30d36d1c7b5ba7`
-  //     )
-  //     .then((res) => {
-  //       console.log(res);
-  //       setMovieList(res.data.results);
-  //     });
-  // }, [type]);
-
   useEffect(() => {
     localStorage.setItem("favorite", JSON.stringify(favorite));
   }, [favorite]);
@@ -95,6 +73,7 @@ const MoviePage = () => {
                     target="_blank"
                   >
                     <img
+                      lazy
                       className="movieimg"
                       src={`https://image.tmdb.org/t/p/original/${post.poster_path}`}
                     />
@@ -180,100 +159,6 @@ const MoviePage = () => {
               </div>
             )
           )}
-
-          {/* {movieList.map((post) => (
-            <div className="moviebox">
-              <Link to={`/movie/${post.id}`} style={{ textDecoration: "none" }}>
-                <a
-                  style={{ textDecoration: "none", color: "#fff" }}
-                  href={`https://image.tmdb.org/t/p/original/${post.poster_path}`}
-                  target="_blank"
-                >
-                  <img
-                    className="movieimg"
-                    src={`https://image.tmdb.org/t/p/original/${post.poster_path}`}
-                  />
-                  <p className="caption">{`${post.original_title}`}</p>
-                  <div className="star-rating">
-                    {post.vote_average >= 2 ? (
-                      <img
-                        width="20px"
-                        src="https://img.uxwing.com/wp-content/themes/uxwing/download/arts-graphic-shapes/star-icon.svg"
-                      />
-                    ) : (
-                      <img
-                        width="20px"
-                        src="https://img.uxwing.com/wp-content/themes/uxwing/download/arts-graphic-shapes/star-full-icon.svg"
-                      />
-                    )}
-                    {post.vote_average >= 4 ? (
-                      <img
-                        width="20px"
-                        src="https://img.uxwing.com/wp-content/themes/uxwing/download/arts-graphic-shapes/star-icon.svg"
-                      />
-                    ) : (
-                      <img
-                        width="20px"
-                        src="https://img.uxwing.com/wp-content/themes/uxwing/download/arts-graphic-shapes/star-full-icon.svg"
-                      />
-                    )}
-                    {post.vote_average >= 6 ? (
-                      <img
-                        width="20px"
-                        src="https://img.uxwing.com/wp-content/themes/uxwing/download/arts-graphic-shapes/star-icon.svg"
-                      />
-                    ) : (
-                      <img
-                        width="20px"
-                        src="https://img.uxwing.com/wp-content/themes/uxwing/download/arts-graphic-shapes/star-full-icon.svg"
-                      />
-                    )}
-                    {post.vote_average >= 8 ? (
-                      <img
-                        width="20px"
-                        src="https://img.uxwing.com/wp-content/themes/uxwing/download/arts-graphic-shapes/star-icon.svg"
-                      />
-                    ) : (
-                      <img
-                        width="20px"
-                        src="https://img.uxwing.com/wp-content/themes/uxwing/download/arts-graphic-shapes/star-full-icon.svg"
-                      />
-                    )}
-                    {post.vote_average >= 10 ? (
-                      <img
-                        width="20px"
-                        src="https://img.uxwing.com/wp-content/themes/uxwing/download/arts-graphic-shapes/star-icon.svg"
-                      />
-                    ) : (
-                      <img
-                        width="20px"
-                        src="https://img.uxwing.com/wp-content/themes/uxwing/download/arts-graphic-shapes/star-full-icon.svg"
-                      />
-                    )}
-                    {post.vote_average !== 0 && ` (${post.vote_average})`}
-                  </div>
-                </a>
-              </Link>
-              <Link>
-                <button
-                  className="heart_btn"
-                  onClick={() => heartFunction(post.id)}
-                >
-                  {favorite.includes(post.id) ? (
-                    <img
-                      className="heart_img"
-                      src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/42/Love_Heart_SVG.svg/968px-Love_Heart_SVG.svg.png"
-                    />
-                  ) : (
-                    <img
-                      className="heart_img_black"
-                      src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/42/Love_Heart_SVG.svg/968px-Love_Heart_SVG.svg.png"
-                    />
-                  )}
-                </button>
-              </Link>
-            </div>
-          ))} */}
         </div>
       </div>
       <div className="prev-next-parent">
